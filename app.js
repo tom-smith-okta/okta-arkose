9bc197c9ac8e43993992f8067cd5fa88a5c2b6b7
Tom Smith <tom.smith@okta.com>
2019-02-28 19:53:42 -0500

    flow works

--- app.js
+++ app.js
@@ -6,9 +6,12 @@ const bodyParser = require("body-parser")
 
 const express = require('express')
 
-var cors = require('cors')
+// var cors = require('cors')
 
 const request = require("request")
+
+var OktaAuth = require('@okta/okta-auth-js');
+
 
 ///////////////////////////////////////////////////
 
@@ -27,251 +30,317 @@ app.listen(port, function () {
 	console.log('App listening on port ' + port + '...');
 })
 
+///////////////////////////////////////////////////
+
+
+var config = {
+	url: "https://okta-arkose.oktapreview.com"
+}
+
+var authClient = new OktaAuth(config);
+
+
+
+
+app.post('/authn', function (req, res) {
+
+	authClient.signIn({
+	  username: req.body.username,
+	  password: req.body.password
+	})
+	.then(function(transaction) {
+	  if (transaction.status === 'SUCCESS') {
+	    // authClient.session.setCookieAndRedirect(transaction.sessionToken); // Sets a cookie on redirect
+
+	    console.log("the transaction is: ")
+
+	    console.dir(transaction)
+
+	    res.send(transaction)
+
+	    // res.send(transaction.sessionToken)
+
+	    // console.log("redirecting...")
+
+	    // res.redirect("http://localhost:3378")
+
+	    // res.redirect("http://localhost:3378")
+
+	  } else {
+	    throw 'We cannot handle the ' + transaction.status + ' status';
+	  }
+	})
+	.fail(function(err) {
+	  console.error(err);
+	});
+
+})
+
+
+
 /******************************************************/
 // CORS stuff
 
-var whitelist = [ process.env.DOMAIN ]
+// var whitelist = [ process.env.DOMAIN ]
 
-var corsOptions = {
-	origin: function (origin, callback) {
+// var corsOptions = {
+// 	origin: function (origin, callback) {
 
-		if (whitelist.indexOf(origin) !== -1 || !origin) {
-			callback(null, true)
-		} else {
-			callback(new Error('Not allowed by CORS'))
-		}
-	}
-}
+// 		if (whitelist.indexOf(origin) !== -1 || !origin) {
+// 			callback(null, true)
+// 		} else {
+// 			callback(new Error('Not allowed by CORS'))
+// 		}
+// 	}
+// }
 
-app.options("/*", function(req, res, next){
-	res.header('Access-Control-Allow-Origin', process.env.DOMAIN);
-	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-	res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-	res.header('Access-Control-Allow-Credentials', true)
-	res.header("Access-Control-Allow-Headers", "x-okta-user-agent-extended, x-okta-xsrftoken, content-type");
-	res.sendStatus(200);
-})
+// app.options("/*", function(req, res, next){
+// 	res.header('Access-Control-Allow-Origin', process.env.DOMAIN);
+// 	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+// 	res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+// 	res.header('Access-Control-Allow-Credentials', true)
+// 	res.header("Access-Control-Allow-Headers", "x-okta-user-agent-extended, x-okta-xsrftoken, content-type");
+// 	res.sendStatus(200);
+// })
 
 /*********************************************************/
+// app.post('/authn', cors(corsOptions), function (req, res) {
 
-app.post('/api/v1/authn', cors(corsOptions), function (req, res) {
+
+// app.post('/authn', function (req, res) {
 	
-	res.header('Access-Control-Allow-Credentials', true)
+// 	// res.header('Access-Control-Allow-Credentials', true)
 
-	console.log("the request body is: ")
+// 	console.log("the request body is: ")
 
-	console.dir(req.body)
+// 	console.dir(req.body)
 
-	var err = {
-		errorCode: "E0000004",
-		errorSummary: "Authentication failed",
-		errorLink: "E0000004",
-		errorId: "",
-		errorCauses: []
-	}
+// 	var err = {
+// 		errorCode: "E0000004",
+// 		errorSummary: "Authentication failed",
+// 		errorLink: "E0000004",
+// 		errorId: "",
+// 		errorCauses: []
+// 	}
 
-	// Check for the context token
-	// but don't redeem the context token until after Okta authn
-	// if user mis-types password they can still use the same context token
+// 	// Check for the context token
+// 	// but don't redeem the context token until after Okta authn
+// 	// if user mis-types password they can still use the same context token
 
-	if (!req.body.contextData) {
-		res.status(401).json(err)
-	}
-	else {
+// 	if (!req.body.contextData) {
+// 		err.errorCauses.push("No context token included.")
 
-		var username = req.body.username
+// 		res.status(401).json(err)
+// 	}
+// 	else {
 
-		// get the Okta user id
-		getUserID(username, function(error, userID) {
+// 		var username = req.body.username
 
-			if (error) {
-				err.errorCauses.push("Could not find Okta ID for this user.")
-				res.status(401).json(err)
-			}
+// 		// get the Okta user id
+// 		getUserID(username, function(error, userID) {
 
-			console.log("the user id is: " + userID)
+// 			if (error) {
+// 				err.errorCauses.push("Could not find Okta ID for this user.")
+// 				res.status(401).json(err)
+// 			}
 
-			// move the user into a group that allows authn
-			unlockUser(userID, function(error) {
+// 			console.log("the user id is: " + userID)
 
-				if (error) {
-					err.errorCauses.push("Could not move Okta user into authn group.")
-					res.status(401).json(err)							
-				}
+// 			// move the user into a group that allows authn
+// 			unlockUser(userID, function(error) {
 
-				console.log("user moved into open group.")
+// 				if (error) {
+// 					err.errorCauses.push("Could not move Okta user into authn group.")
+// 					res.status(401).json(err)							
+// 				}
 
-				// attempt to authenticate the user vs. okta
-				authenticateUser(username, req.body.password, function(error, statusCode, body) {
+// 				console.log("user moved into open group.")
+
+// 				// attempt to authenticate the user vs. okta
+// 				authenticateUser(username, req.body.password, function(error, statusCode, body) {
 				
-					if (error) {
-						res.status(401).json(err)							
-					}
+// 					if (error) {
+// 						res.status(401).json(err)							
+// 					}
 
-					console.log("the status code is " + statusCode)
-					console.log("the body is: " + JSON.stringify(body))
+// 					console.log("the status code is " + statusCode)
+// 					console.log("the body is: " + JSON.stringify(body))
 
-					if (statusCode == 401) { // authn vs. okta failed
-						res.status(statusCode).json(body)
-					}
+// 					if (statusCode == 401) { // authn vs. okta failed
+// 						res.status(statusCode).json(body)
+// 					}
 
-					else {
+// 					else {
 
-						var contextToken = req.body.contextData
+// 						var contextToken = req.body.contextData
 
-						redeemContextToken(contextToken, function(error, response) {
+// 						redeemContextToken(contextToken, function(error, response) {
 
-							if (!response.solved) {
-								err.errorCauses.push("Context token did not pass inspection.")
-								res.status(401).json(err)
-							}
-							else {
-								res.status(statusCode).json(body)
-							}
-						})						
-					}
+// 							if (!response.solved) {
+// 								err.errorCauses.push("Context token did not pass inspection.")
+// 								res.status(401).json(err)
+// 							}
+// 							else {
+// 								res.status(statusCode).json(body)
 
-					// remove the user from the open group
-					lockUser(userID, function(error) {
+// 								// var oauth_url = "https://okta-arkose.oktapreview.com/oauth2/v1/authorize"
 
-						if (error) { console.log(error) }
+// 								// oauth_url += "?client_id=0oajdyrcz6yCh60oG0h7"
+// 								// oauth_url += "&response_type=id_token"
+// 								// oauth_url += "&scope=openid&prompt=none"
+// 								// oauth_url += "&redirect_uri=http://localhost:3378"
+// 								// oauth_url += "&state=Af0ifjslDkj&nonce=n-0S6_WzA2Mj"
+// 								// oauth_url += "&sessionToken=" + body.sessionToken
 
-					})		
-				})
-			})
-		})
-	}
-})
+// 								// // oauth_url = "http://www.tomgsmith.com"
 
-function authenticateUser(username, password, callback) {
+// 								// res.redirect(oauth_url)
 
-	var options = {
-		method: 'POST',
-		url: process.env.OKTA_TENANT + '/api/v1/authn',
-		headers: {
-			 'cache-control': 'no-cache',
-			 'Content-Type': 'application/json',
-			 Accept: 'application/json'
-		},
-		body: {
-			username: username,
-			password: password,
-			options: {
-				multiOptionalFactorEnroll: false,
-				warnBeforePasswordExpired: true
-			}
-		},
-		json: true
-	}
+// 							}
+// 						})						
+// 					}
 
-	request(options, function (error, response, body) {
+// 					// remove the user from the open group
+// 					lockUser(userID, function(error) {
 
-		if (error) return callback(error)
+// 						if (error) { console.log(error) }
 
-		console.log("the response from Okta is: ")
+// 					})		
+// 				})
+// 			})
+// 		})
+// 	}
+// })
 
-		console.dir(body)
+// function authenticateUser(username, password, callback) {
 
-		var status
+// 	var options = {
+// 		method: 'POST',
+// 		url: process.env.OKTA_TENANT + '/api/v1/authn',
+// 		headers: {
+// 			 'cache-control': 'no-cache',
+// 			 'Content-Type': 'application/json',
+// 			 Accept: 'application/json'
+// 		},
+// 		body: {
+// 			username: username,
+// 			password: password,
+// 			options: {
+// 				multiOptionalFactorEnroll: false,
+// 				warnBeforePasswordExpired: true
+// 			}
+// 		},
+// 		json: true
+// 	}
 
-		if (body.status == "SUCCESS") { status = 200 }
-		else { status = 401 }
+// 	request(options, function (error, response, body) {
 
-		return callback(null, status, body)
-	})
-}
+// 		if (error) return callback(error)
 
-function getUserID(username, callback) {
+// 		console.log("the response from Okta is: ")
 
-	var options = {
-		method: 'GET',
-		url: process.env.OKTA_TENANT + '/api/v1/users/' + username,
-	 	headers: {
-	    	'cache-control': 'no-cache',
-	     	Authorization: 'SSWS ' + process.env.OKTA_API_TOKEN,
-	     	'Content-Type': 'application/json',
-	     	Accept: 'application/json'
-	    }
-	}
+// 		console.dir(body)
 
-	request(options, function (error, response, body) {
-	  	console.log(body)
+// 		var status
 
-		var obj = JSON.parse(body)
+// 		if (body.status == "SUCCESS") { status = 200 }
+// 		else { status = 401 }
 
-		if (error) { return callback(error) }
+// 		return callback(null, status, body)
+// 	})
+// }
 
-		if (obj.errorCode) { return callback(obj.errorCode) }
+// function getUserID(username, callback) {
 
-		return callback(null, obj.id)
-	})
-}
+// 	var options = {
+// 		method: 'GET',
+// 		url: process.env.OKTA_TENANT + '/api/v1/users/' + username,
+// 	 	headers: {
+// 	    	'cache-control': 'no-cache',
+// 	     	Authorization: 'SSWS ' + process.env.OKTA_API_TOKEN,
+// 	     	'Content-Type': 'application/json',
+// 	     	Accept: 'application/json'
+// 	    }
+// 	}
 
-function lockUser(userID, callback) {
+// 	request(options, function (error, response, body) {
+// 	  	console.log(body)
 
-	var options = {
-		method: 'DELETE',
- 		url: process.env.OKTA_TENANT + '/api/v1/groups/' + process.env.OKTA_OPEN_GROUP_ID + '/users/' + userID,
-		headers: {
-    		'cache-control': 'no-cache',
-    		Authorization: 'SSWS ' + process.env.OKTA_API_TOKEN,
-    		'Content-Type': 'application/json',
-    		Accept: 'application/json'
-    	}
-    }
+// 		var obj = JSON.parse(body)
 
-	request(options, function (error, response, body) {
-		if (error) return callback(error)
+// 		if (error) { return callback(error) }
 
-	 	console.log(body)
-	})
-}
+// 		if (obj.errorCode) { return callback(obj.errorCode) }
 
-function redeemContextToken(contextToken, callback) {
+// 		return callback(null, obj.id)
+// 	})
+// }
 
-	var url = "https://verify.arkoselabs.com/fc/v/"
-	url += "?private_key=" + process.env.PRIVATE_KEY
-	url += "&session_token=" + contextToken
+// function lockUser(userID, callback) {
 
-	var options = {
-		method: 'GET',
-	  	url: url,
-	  	headers: {
-	    	'cache-control': 'no-cache',
-	    	'Content-Type': 'application/json',
-	    	Accept: 'application/json'
-	    }
-	}
+// 	var options = {
+// 		method: 'DELETE',
+//  		url: process.env.OKTA_TENANT + '/api/v1/groups/' + process.env.OKTA_OPEN_GROUP_ID + '/users/' + userID,
+// 		headers: {
+//     		'cache-control': 'no-cache',
+//     		Authorization: 'SSWS ' + process.env.OKTA_API_TOKEN,
+//     		'Content-Type': 'application/json',
+//     		Accept: 'application/json'
+//     	}
+//     }
 
-	request(options, function (error, response, body) {
-	 	if (error) throw new Error(error)
+// 	request(options, function (error, response, body) {
+// 		if (error) return callback(error)
 
-	 	console.log(body)
+// 	 	console.log(body)
+// 	})
+// }
 
-	 	return callback(null, JSON.parse(body))
-	})
-}
+// function redeemContextToken(contextToken, callback) {
 
-function unlockUser(userID, callback) {
+// 	var url = "https://verify.arkoselabs.com/fc/v/"
+// 	url += "?private_key=" + process.env.ARKOSE_PRIVATE_KEY
+// 	url += "&session_token=" + contextToken
 
-	var options = {
-		method: 'PUT',
-		url: process.env.OKTA_TENANT + '/api/v1/groups/' + process.env.OKTA_OPEN_GROUP_ID + '/users/' + userID,
-		headers: {
-			'cache-control': 'no-cache',
-	    	Authorization: 'SSWS ' + process.env.OKTA_API_TOKEN,
-	    	'Content-Type': 'application/json',
-	    	Accept: 'application/json'
-	    }
-	}
+// 	var options = {
+// 		method: 'GET',
+// 	  	url: url,
+// 	  	headers: {
+// 	    	'cache-control': 'no-cache',
+// 	    	'Content-Type': 'application/json',
+// 	    	Accept: 'application/json'
+// 	    }
+// 	}
 
-	request(options, function (error, response, body) {
-		if (error) { return callback(error) }
+// 	request(options, function (error, response, body) {
+// 	 	if (error) throw new Error(error)
 
-		if (response.statusCode != 204) {
-			return callback("error")
-		}
+// 	 	console.log(body)
 
-		return callback(null)
-	})
-}
+// 	 	return callback(null, JSON.parse(body))
+// 	})
+// }
+
+// function unlockUser(userID, callback) {
+
+// 	var options = {
+// 		method: 'PUT',
+// 		url: process.env.OKTA_TENANT + '/api/v1/groups/' + process.env.OKTA_OPEN_GROUP_ID + '/users/' + userID,
+// 		headers: {
+// 			'cache-control': 'no-cache',
+// 	    	Authorization: 'SSWS ' + process.env.OKTA_API_TOKEN,
+// 	    	'Content-Type': 'application/json',
+// 	    	Accept: 'application/json'
+// 	    }
+// 	}
+
+// 	request(options, function (error, response, body) {
+// 		if (error) { return callback(error) }
+
+// 		if (response.statusCode != 204) {
+// 			return callback("error")
+// 		}
+
+// 		return callback(null)
+// 	})
+// }
---
+++ appBAK.js
@@ -0,0 +1,295 @@
+////////////////////////////////////////////////////
+
+require('dotenv').config()
+
+const bodyParser = require("body-parser")
+
+const express = require('express')
+
+var cors = require('cors')
+
+const request = require("request")
+
+///////////////////////////////////////////////////
+
+// SET UP WEB SERVER
+const app = express()
+
+var port = process.env.PORT
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use(bodyParser.json())
+
+app.use(express.static('public'))
+
+app.listen(port, function () {
+	console.log('App listening on port ' + port + '...');
+})
+
+/******************************************************/
+// CORS stuff
+
+var whitelist = [ process.env.DOMAIN ]
+
+var corsOptions = {
+	origin: function (origin, callback) {
+
+		if (whitelist.indexOf(origin) !== -1 || !origin) {
+			callback(null, true)
+		} else {
+			callback(new Error('Not allowed by CORS'))
+		}
+	}
+}
+
+app.options("/*", function(req, res, next){
+	res.header('Access-Control-Allow-Origin', process.env.DOMAIN);
+	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+	res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+	res.header('Access-Control-Allow-Credentials', true)
+	res.header("Access-Control-Allow-Headers", "x-okta-user-agent-extended, x-okta-xsrftoken, content-type");
+	res.sendStatus(200);
+})
+
+/*********************************************************/
+// app.post('/authn', cors(corsOptions), function (req, res) {
+
+
+app.post('/authn', function (req, res) {
+	
+	// res.header('Access-Control-Allow-Credentials', true)
+
+	console.log("the request body is: ")
+
+	console.dir(req.body)
+
+	var err = {
+		errorCode: "E0000004",
+		errorSummary: "Authentication failed",
+		errorLink: "E0000004",
+		errorId: "",
+		errorCauses: []
+	}
+
+	// Check for the context token
+	// but don't redeem the context token until after Okta authn
+	// if user mis-types password they can still use the same context token
+
+	if (!req.body.contextData) {
+		err.errorCauses.push("No context token included.")
+
+		res.status(401).json(err)
+	}
+	else {
+
+		var username = req.body.username
+
+		// get the Okta user id
+		getUserID(username, function(error, userID) {
+
+			if (error) {
+				err.errorCauses.push("Could not find Okta ID for this user.")
+				res.status(401).json(err)
+			}
+
+			console.log("the user id is: " + userID)
+
+			// move the user into a group that allows authn
+			unlockUser(userID, function(error) {
+
+				if (error) {
+					err.errorCauses.push("Could not move Okta user into authn group.")
+					res.status(401).json(err)							
+				}
+
+				console.log("user moved into open group.")
+
+				// attempt to authenticate the user vs. okta
+				authenticateUser(username, req.body.password, function(error, statusCode, body) {
+				
+					if (error) {
+						res.status(401).json(err)							
+					}
+
+					console.log("the status code is " + statusCode)
+					console.log("the body is: " + JSON.stringify(body))
+
+					if (statusCode == 401) { // authn vs. okta failed
+						res.status(statusCode).json(body)
+					}
+
+					else {
+
+						var contextToken = req.body.contextData
+
+						redeemContextToken(contextToken, function(error, response) {
+
+							if (!response.solved) {
+								err.errorCauses.push("Context token did not pass inspection.")
+								res.status(401).json(err)
+							}
+							else {
+								res.status(statusCode).json(body)
+
+								// var oauth_url = "https://okta-arkose.oktapreview.com/oauth2/v1/authorize"
+
+								// oauth_url += "?client_id=0oajdyrcz6yCh60oG0h7"
+								// oauth_url += "&response_type=id_token"
+								// oauth_url += "&scope=openid&prompt=none"
+								// oauth_url += "&redirect_uri=http://localhost:3378"
+								// oauth_url += "&state=Af0ifjslDkj&nonce=n-0S6_WzA2Mj"
+								// oauth_url += "&sessionToken=" + body.sessionToken
+
+								// // oauth_url = "http://www.tomgsmith.com"
+
+								// res.redirect(oauth_url)
+
+							}
+						})						
+					}
+
+					// remove the user from the open group
+					lockUser(userID, function(error) {
+
+						if (error) { console.log(error) }
+
+					})		
+				})
+			})
+		})
+	}
+})
+
+function authenticateUser(username, password, callback) {
+
+	var options = {
+		method: 'POST',
+		url: process.env.OKTA_TENANT + '/api/v1/authn',
+		headers: {
+			 'cache-control': 'no-cache',
+			 'Content-Type': 'application/json',
+			 Accept: 'application/json'
+		},
+		body: {
+			username: username,
+			password: password,
+			options: {
+				multiOptionalFactorEnroll: false,
+				warnBeforePasswordExpired: true
+			}
+		},
+		json: true
+	}
+
+	request(options, function (error, response, body) {
+
+		if (error) return callback(error)
+
+		console.log("the response from Okta is: ")
+
+		console.dir(body)
+
+		var status
+
+		if (body.status == "SUCCESS") { status = 200 }
+		else { status = 401 }
+
+		return callback(null, status, body)
+	})
+}
+
+function getUserID(username, callback) {
+
+	var options = {
+		method: 'GET',
+		url: process.env.OKTA_TENANT + '/api/v1/users/' + username,
+	 	headers: {
+	    	'cache-control': 'no-cache',
+	     	Authorization: 'SSWS ' + process.env.OKTA_API_TOKEN,
+	     	'Content-Type': 'application/json',
+	     	Accept: 'application/json'
+	    }
+	}
+
+	request(options, function (error, response, body) {
+	  	console.log(body)
+
+		var obj = JSON.parse(body)
+
+		if (error) { return callback(error) }
+
+		if (obj.errorCode) { return callback(obj.errorCode) }
+
+		return callback(null, obj.id)
+	})
+}
+
+function lockUser(userID, callback) {
+
+	var options = {
+		method: 'DELETE',
+ 		url: process.env.OKTA_TENANT + '/api/v1/groups/' + process.env.OKTA_OPEN_GROUP_ID + '/users/' + userID,
+		headers: {
+    		'cache-control': 'no-cache',
+    		Authorization: 'SSWS ' + process.env.OKTA_API_TOKEN,
+    		'Content-Type': 'application/json',
+    		Accept: 'application/json'
+    	}
+    }
+
+	request(options, function (error, response, body) {
+		if (error) return callback(error)
+
+	 	console.log(body)
+	})
+}
+
+function redeemContextToken(contextToken, callback) {
+
+	var url = "https://verify.arkoselabs.com/fc/v/"
+	url += "?private_key=" + process.env.ARKOSE_PRIVATE_KEY
+	url += "&session_token=" + contextToken
+
+	var options = {
+		method: 'GET',
+	  	url: url,
+	  	headers: {
+	    	'cache-control': 'no-cache',
+	    	'Content-Type': 'application/json',
+	    	Accept: 'application/json'
+	    }
+	}
+
+	request(options, function (error, response, body) {
+	 	if (error) throw new Error(error)
+
+	 	console.log(body)
+
+	 	return callback(null, JSON.parse(body))
+	})
+}
+
+function unlockUser(userID, callback) {
+
+	var options = {
+		method: 'PUT',
+		url: process.env.OKTA_TENANT + '/api/v1/groups/' + process.env.OKTA_OPEN_GROUP_ID + '/users/' + userID,
+		headers: {
+			'cache-control': 'no-cache',
+	    	Authorization: 'SSWS ' + process.env.OKTA_API_TOKEN,
+	    	'Content-Type': 'application/json',
+	    	Accept: 'application/json'
+	    }
+	}
+
+	request(options, function (error, response, body) {
+		if (error) { return callback(error) }
+
+		if (response.statusCode != 204) {
+			return callback("error")
+		}
+
+		return callback(null)
+	})
+}
